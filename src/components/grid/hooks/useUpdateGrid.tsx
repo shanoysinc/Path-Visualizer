0c70529cd73d3cb29d9697fcaa2a9d3969d2dd6e
shanoy <shanoy96@gmail.com>
2021-07-11 09:13:42 -0700

    fetch update grid

--- src/components/grid/hooks/useUpdateGrid.tsx
+++ src/components/grid/hooks/useUpdateGrid.tsx
@@ -1,14 +1,15 @@
 import { useRecoilCallback } from "recoil";
 import { GridNode } from "../../../algorithms/graph/dijkstra";
 import { NodeAtom } from "../../../state/pathFinder/atoms";
+import { NUMBER_OF_COLS, NUMBER_OF_ROWS } from "./useInitialGrid";
 
 export function useUpdateGrid() {
   return useRecoilCallback(({ snapshot }) => async () => {
     const updatedGrid: GridNode[][] = [];
 
-    for (let row = 0; row < 20; row++) {
+    for (let row = 0; row < NUMBER_OF_ROWS; row++) {
       let columns = [];
-      for (let col = 0; col < 40; col++) {
+      for (let col = 0; col < NUMBER_OF_COLS; col++) {
         const node = await snapshot.getPromise(NodeAtom({ row, col }));
         columns.push(node);
       }
