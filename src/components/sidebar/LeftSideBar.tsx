e3561e03f8aedde11a9d5ac14373147482b5c09d
shanoy <shanoy96@gmail.com>
2021-07-12 12:51:55 -0700

    add sidebar

---
+++ src/components/sidebar/LeftSideBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { SidebarContent } from "./components";
+const LeftSideBar = () => {
+  return (
+    <>
+      <SidebarContent />
+    </>
+  );
+};
+
+export default LeftSideBar;
