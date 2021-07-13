0356164f5b4e19b3de59de03b62e0227841057b2
shanoy <shanoy96@gmail.com>
2021-07-12 18:50:59 -0700

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,11 @@ import GridTest from "./components/grid/GridTest";
 import GridKeys from "./components/GridKeys/GridKeys";
 import Drawer from "./components/drawer/Drawer";
 import LeftSideBar from "./components/sidebar/LeftSideBar";
+import { useRecoilState } from "recoil";
+import { GridAtom } from "./state/pathFinder/atoms";
 function App() {
+  // const [grid, setGrid] = useRecoilState(GridAtom);
+
   return (
     <div className="App">
       {/* <Drawer /> */}
