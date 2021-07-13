e272937a6ee2069b923fe89c08db7b2b3848041a
shanoy <shanoy96@gmail.com>
2021-07-12 19:50:16 -0700

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,12 @@ import React from "react";
 import "./App.css";
 import { Grid as VisualizerGrid } from "./components";
 import { Grid, GridItem } from "@chakra-ui/react";
-import GridTest from "./components/grid/GridTest";
 import GridKeys from "./components/GridKeys/GridKeys";
-import Drawer from "./components/drawer/Drawer";
 import LeftSideBar from "./components/sidebar/LeftSideBar";
-import { useRecoilState } from "recoil";
-import { GridAtom } from "./state/pathFinder/atoms";
-function App() {
-  // const [grid, setGrid] = useRecoilState(GridAtom);
 
+function App() {
   return (
     <div className="App">
-      {/* <Drawer /> */}
       <Grid
         h="100vh"
         templateRows="repeat(5, 1fr)"
@@ -31,7 +25,6 @@ function App() {
 
         <GridItem rowGap={2} rowSpan={5} colSpan={3}>
           <VisualizerGrid />
-          {/* <GridTest /> */}
         </GridItem>
       </Grid>
     </div>
