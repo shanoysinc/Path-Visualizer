0884185f9281f80f95f1f0756342caa7f3c97ce9
shanoy <shanoy96@gmail.com>
2021-07-12 18:49:01 -0700

    switch around icons

--- src/components/grid/components/node/Node.tsx
+++ src/components/grid/components/node/Node.tsx
@@ -135,8 +135,8 @@ export const Node = memo(({ col, row }: Props) => {
       draggable={false}
       className={`grid__node ${isStartNode} ${isEndNode} ${wall}`}
     >
-      {isEndNode && <ArrowRightIcon w={4} h={4} color="#5b21b6" />}
-      {isStartNode && <StarIcon w={5} h={5} color="yellow.400" />}
+      {isStartNode && <ArrowRightIcon w={4} h={4} color="#5b21b6" />}
+      {isEndNode && <StarIcon w={5} h={5} color="yellow.400" />}
     </div>
   );
 });
