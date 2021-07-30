9c6cc2714f87e155bba0fd652e95b2c4e07aafb1
shanoy <shanoy96@gmail.com>
2021-07-30 12:49:20 -0700

    use sync state instead of async

--- src/components/grid/hooks/useUpdateGrid.tsx
+++ src/components/grid/hooks/useUpdateGrid.tsx
@@ -3,13 +3,14 @@ import { GridNode } from "../../../algorithms/graph/";
 import { NodeAtom } from "../../../state/pathFinder/atoms";
 import { grid } from "./useInitialGrid";
 export function useUpdateGrid() {
-  return useRecoilCallback(({ snapshot }) => async () => {
+  return useRecoilCallback(({ snapshot }) => () => {
     const updatedGrid: GridNode[][] = [];
 
     for (let row = 0; row < grid.length; row++) {
       const columns = [];
       for (let col = 0; col < grid[row].length; col++) {
-        const node = await snapshot.getPromise(NodeAtom({ row, col }));
+        const state = snapshot.getLoadable<GridNode>(NodeAtom({ row, col }));
+        const node = state.getValue();
         columns.push(node);
       }
       updatedGrid.push(columns);
